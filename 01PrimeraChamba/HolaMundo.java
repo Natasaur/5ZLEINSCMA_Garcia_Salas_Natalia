c352982a96a3ec8cbe2a10678ea595305621fea9
Natasaur <gatoslibres2231@gmail.com>
2024-01-16 11:04:07 -0600

    Primera clase

---
+++ 01PrimeraChamba/HolaMundo.java
@@ -0,0 +1,25 @@
+//Vamos a creau un primer programa
+/*Comentario de varias lineas */
+
+//Toda clase debe tener el mismo nombre que el archivo
+
+public class HolaMundo {
+    //Toda clase debe tener un método principal
+    //Los metodos son los comportamientos que va a tener una clase u objeto
+    //Los comportamientso son las acciones u operaciones para las que está destinada la clase
+    /*
+     * Metodos principales - (main)
+     * metodos getter and setter - intercambio de datos
+     * metodos constructores - se encargan de la estructura de la clase, inicializar las variables y crear las instancias
+     * metodos por tipo de dato - int, float, void, string, etc
+     */
+
+     /*Los objetos son instancias (invocaciones - llamado) la construccion abstracta del objeto
+     Las caracteristicas del objeto son los atributos de la clase
+     los metodos son las habilidades o funciones del objeto      
+     */
+    public static void main(String[] args) {
+        //argumento = valores con los que trabaja el metodo
+        System.out.println("Si sale error, matense");
+    }
+}
