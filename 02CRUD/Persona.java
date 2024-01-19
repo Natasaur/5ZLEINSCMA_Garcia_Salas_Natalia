0adf3dc348762afcdc5dbace08846a5716afe85d
Natasaur <gatoslibres2231@gmail.com>
2024-01-19 08:59:16 -0600

    19/01/24

---
+++ 02CRUD/Persona.java
@@ -0,0 +1,39 @@
+/*
+ * Vamos a crear una aplicacion para poder tener una plantilla de registro
+ */
+package 02CRUD;
+
+public class Persona {
+    private int id;
+    private String nombre;
+    private int edad;
+}
+
+//constructor
+/*inicializa las variables
+ * define la instancia del objeto
+ * tipos de constructor n
+ * sobrecarga, crear tantos constructores como sean, nombrar de la misma forma a u objeto pero con diferentes párametros
+ */
+public Persona(int id, String nombre, int edad){
+    //this hace referencia al propio objeto
+    this.id = id;
+    this.nombre = nombre;
+    this.edad = edad;
+}
+
+public int getId(){
+    return id;
+}
+public void setId(int id){
+    this.id = id;
+}
+
+//funcion retorna un resultado, el método puede o no dar un resultado
+
+/*
+ * privado = solo se puede acceder desde la misma clase
+ * modificadores privados = private(), private static(no va a ser alterado, solo a traves de get y set), private static final(no puede ser modificado)
+ * protegido puede ser heredado, privado no
+ * 
+ */
