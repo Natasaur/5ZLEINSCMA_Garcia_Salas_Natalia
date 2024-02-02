9b1d4e793088f2ea37b6d250479154af26fb953c
Natasaur <gatoslibres2231@gmail.com>
2024-02-02 14:39:28 -0600

    Clase 2/02/24

--- 03Serializacion/src/pkg03serializacion/Funciones.java
+++ 03Serializacion/src/pkg03serializacion/Funciones.java
@@ -58,20 +58,53 @@ public class Funciones {
             }catch (Exception e){
                 System.out.println("Error: " + e.getMessage());
             }
-            
         }
     }
+    
     private void pedirDatosAlumno(){
-    bol = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la boleta: "));
-    edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la edad: "));
-    nombre = JOptionPane.showInputDialog("Ingresa el nombre: ");
-    //Son los datos del objeto creado en la línea 49
+        bol = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la boleta: "));
+        edad = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la edad: "));
+        nombre = JOptionPane.showInputDialog("Ingresa el nombre: ");
+        //Son los datos del objeto creado en la línea 49
     }
     
     private void guardarAlumno(){
         listapersonas.add(alumno);
     }
     
-}
-
-
+    //Método para leer un archivo
+    private void leerAlumnos() throws Exception{
+        //1. Definir dónde está el archivo que guarda la información
+        FileInputStream archivo = new FileInputStream("archivo.dat");
+        /*
+         * Estar vacío
+         * Que no exista
+         * Que tenga un error
+         */
+        //El archivo guarda el objeto listadepersonas
+        ObjectInputStream entrada = new ObjectInputStream(archivo); //Para leer los objetos  del archivo
+        /**
+         * Tenemos que transformar los objectos por que por defecto su lectura
+        es en bytes por lo tanto debo castear un objeto
+        */
+        listapersonas = (ArrayList)entrada.readObject(); //La lectura se transforma en un objeto de tipo ArrayList
+        for(int i = 0; i < listapersonas.size(); i++){
+            //Recorro lo que necesito del objeto
+            Alumno obj = (Alumno)listapersonas.get(i);
+            //Obtener la información
+            JOptionPane.showMessageDialog(null,"\n"
+            + "No. Alumno: " + (i+1) + "\n"
+            + "Boleta: " + obj.getBoleta() + "\n"
+            + "Edad: " + obj.getEdad() + "\n"
+            + "Tipo de Persona: " + obj.tipoPersona() + "\n");
+        }
+    }
+        private void guardar() throws Exception{
+            //Debo crear primero el archivo de salida
+            FileOutputStream archivo = new FileOutputStream("archivo.dat");
+            ObjectOutputStream salida = new ObjectOutputStream(archivo);
+            //Empiezo a escribir
+            salida.writeObject(listapersonas);
+            salida.close();
+        }
+    }
