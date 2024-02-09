95195cc684d9dbafaaf86d2b3e3378f2102607ca
Natasaur <gatoslibres2231@gmail.com>
2024-02-09 09:03:23 -0600

    Clase 09/02/24

--- ConexionBD/src/java/Registro.java
+++ ConexionBD/src/java/Registro.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.*;
+import javax.servlet.ServletConfig;
+
 /**
  *
  * @author jakeg
@@ -25,6 +28,46 @@ public class Registro extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    //Servlet = Código en java que da una salida en html
+    /**
+     * Vamos a conectar a una base de datos 
+    */
+    
+    private Connection con;
+    //com es para establecer el objeto de coneccion
+    private Statement set;
+    //El objeto statement es para poder definir las senctencias sql
+    //Crear una tabla, registrar un dato
+    private ResultSet rs;
+    //Este objeto es exclusivo de consultas
+    
+    //Se crea un tipo de constructor
+    public void init(ServletConfig scg) throws ServletException{ //ServletException porque no maneja una Exception normal
+        //sirve para configurar el servicio
+        //En este caso el servicio de la base de datos
+        String url = "jdbc:mysql:3306//localhost/registroalumnos"; //java data base controller
+                    //controlador:motorbd:puerto//IP/nombrebd
+        String username = "";
+        String password = "";
+        
+        try{
+            //Vamos a intentar conectarnos a la base de datos
+            Class.forName("com.mysql.jdbc.Driver");
+            //Se debe obtener el objeto de coneccion
+            con = DriverManager.getConnection(url,username,password);
+            set = com.createStatement();
+            
+            System.out.println("Conexion exitosa!");
+        }catch(Exception e){
+            System.out.println("no se conecto");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        }
+        
+    }
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
